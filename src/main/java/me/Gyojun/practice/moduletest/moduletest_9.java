73ec330c6bd7942f7e5eaa069183a4d783df6ead
장교준 <wkddn2003@gmail.com>
2025-02-10 08:36:20 +0900

    모듈테스트 9번

---
+++ src/main/java/me/Gyojun/practice/moduletest/moduletest_9.java
@@ -0,0 +1,13 @@
+package main.java.me.Gyojun.practice.moduletest;
+
+public class moduletest_9 {
+    public static void main(String[] args) {
+
+        int a = 123;
+        int b = 10;
+        int c = (int)Math.ceil((float)a/10)-b;
+
+        System.out.println(c);
+
+    }
+}
