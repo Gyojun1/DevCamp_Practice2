fec9d62441d7baf0c3e1509ec812f07dc79f3689
장교준 <wkddn2003@gmail.com>
2025-02-10 08:36:50 +0900

    모듈테스트 4번

---
+++ src/main/java/me/Gyojun/practice/moduletest/moduletest_8.java
@@ -0,0 +1,19 @@
+package main.java.me.Gyojun.practice.moduletest;
+
+public class moduletest_8 {
+    public static void main(String[] args) {
+
+        int a=0;
+
+        for(int i=1; i<4; i++){
+            for(int j=1; j<4; j++){
+                a++;
+                System.out.printf("%d %d",a,j);
+                System.out.println();
+
+            }
+        }
+
+
+    }
+}
