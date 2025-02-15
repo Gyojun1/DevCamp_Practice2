793428c17b7b6ce9d809fae208e6cdba78479b1f
장교준 <wkddn2003@gmail.com>
2025-02-15 19:13:54 +0900

    백준 문제

---
+++ src/main/java/me/Gyojun/practice/practice_1/Baek1_1.java
@@ -0,0 +1,26 @@
+package main.java.me.Gyojun.practice.practice_1;
+
+import java.util.Scanner;
+
+public class Baek1_1 {
+
+    public static void main(String[] args) {
+
+        int A;
+        int B;
+
+        Scanner s = new Scanner(System.in);
+        A = s.nextInt();
+        B = s.nextInt();
+
+        System.out.println(A+B);
+
+    }
+
+
+}
+
+
+
+
+
