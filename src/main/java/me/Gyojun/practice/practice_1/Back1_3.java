b28e2e09ddcdf4678b4e65df8b6f46992e024134
장교준 <wkddn2003@gmail.com>
2025-02-23 21:29:33 +0900

    백준

---
+++ src/main/java/me/Gyojun/practice/practice_1/Back1_3.java
@@ -0,0 +1,24 @@
+package main.java.me.Gyojun.practice.practice_1;
+
+
+class Back1_3 {
+
+    public static void main(String[] args){
+
+
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+
+
+
+
+    }
+
+
+}
