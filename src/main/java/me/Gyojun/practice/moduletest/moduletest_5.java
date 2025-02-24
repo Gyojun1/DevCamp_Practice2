585a88d27dc1e9105bb4170a069e089f1d43650d
장교준 <wkddn2003@gmail.com>
2025-02-25 08:10:49 +0900

    백준

---
+++ src/main/java/me/Gyojun/practice/moduletest/moduletest_5.java
@@ -0,0 +1,17 @@
+package main.java.me.Gyojun.practice.moduletest;
+
+public class moduletest_5 {
+    public static void main(String[] args) {
+
+
+        for(int i =0; i<50; i++){
+
+            int a = (int)(Math.random()*11)-5;
+            System.out.println(a);
+
+        }
+
+
+
+    }
+}
